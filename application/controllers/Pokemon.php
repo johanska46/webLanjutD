088749b8af357b914b62a72c556777c73759b46e
Febri Yohanes Aldi <febri.yohanes46@gmail.com>
2016-10-11 11:53:17 +0700

    Update Pokemon.php

--- application/controllers/Pokemon.php
+++ application/controllers/Pokemon.php
@@ -44,9 +44,15 @@ class Pokemon extends CI_Controller{
     public function update_action($id){
         $nama = $this->input->post('nama'); // dapatkan nilai inputan nama
         $tipe = $this->input->post('tipe'); // dapatkan nilai inputan tipe
-        // TODO: panggil fungsi update di pokemon_model
-        // TODO: load view pokemon_update_action
         
+        $data = array(
+                'nama'=>$nama,
+                'tipe'=> $tipe             
+        );
+        // TODO: panggil fungsi update di pokemon_model
+        $this->pokemon_model->pokemon_update_action($id,$data);
+        // TODO: load view pokemon_update_action
+        $this->load->view('pokemon_update_action');
     }
 
     // URL : http://localhost/[directory]/index.php/delete/[id]
